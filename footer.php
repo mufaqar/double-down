9430f3dfee49006dd480701c76bc7a64a821322c
Mufaqar Islam <mufaqar@gmail.com>
2022-07-20 14:38:48 +0500

    Updated Theme

--- footer.php
+++ footer.php
@@ -85,7 +85,7 @@
 <?php wp_footer(); ?>
 
   <!-- bootstrap scripts cdn links  -->
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js"></script>
 
