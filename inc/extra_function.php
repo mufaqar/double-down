95615ee6e1a415689678701366d5eb1e9b5bcff1
Mufaqar Islam <mufaqar@gmail.com>
2022-07-21 13:25:11 +0500

    extra function

---
+++ inc/extra_function.php
@@ -0,0 +1,12 @@
+<?php
+include_once('login.php');
+
+//add_role( 'company', 'Company', array( 'read' => true, 'level_0' => true ) );
+//add_role( 'personal', 'Personal', array( 'read' => true, 'level_0' => true ) );
+
+add_action('after_setup_theme', 'remove_admin_bar');
+function remove_admin_bar() {
+  if (!current_user_can('administrator') && !is_admin()) {
+    show_admin_bar(false);
+  }
+}
