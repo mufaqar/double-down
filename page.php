4d7e513a33658a9f2d6a8e4aeab2536a0d673357
Mufaqar Islam <mufaqar@gmail.com>
2022-07-21 15:05:45 +0500

    page style

--- page.php
+++ page.php
@@ -1,21 +1,35 @@
-<?php get_header(); ?>
-<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-    <div class="post" id="post-<?php the_ID(); ?>">
-        <h1><?php the_title(); ?></h1>
-        <div class="entry">
-           <?php if ( has_post_thumbnail() ) { ?>
-    		    <div class="featured-image">
-					<?php the_post_thumbnail( 'single-post-thumbnail' ); ?>
-    		    </div>
-			<?php } ?>	
-			<?php the_content(); ?>
-			<?php wp_link_pages(array('before' => 'Pages: ', 'next_or_number' => 'number')); ?>
-			<div class="tags">
-				<?php the_tags( __('Tags:','text_domain'),'','.'); ?>
-            </div>
-            <div class="clear"></div>
+<?php get_header('landing'); ?>
+
+
+<main class="main position-relative" style="height: 420px;">
+<div class="container">
+    <div class="d-flex justify-content-between align-content-center align-items-center row">
+        <div class="col-md-6 hero_content" style="padding-top: 10rem !important;">
+            <h1><?php the_title();?></h1>
+          
+        </div>
+        <div class="col-md-6 hero_right">
+            <!-- <img src="<?php bloginfo('template_directory'); ?>/reources/images/right_banner.png" alt="" class="w-100"> -->
         </div>
     </div>
+
+</div>
+       
+</main>
+
+
+<section class="container landing_contents my-5">
+
+
+
+
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+    <div class="post my-5" id="post-<?php the_ID(); ?>">
+             <h1><?php the_title(); ?></h1>       
+			<?php the_content(); ?>			
+    </div>
 <?php endwhile; endif; ?>
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+
+           </section>
+
+<?php get_footer('landing'); ?>
