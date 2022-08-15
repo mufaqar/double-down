f83d855439830625bf9e8f9b2a4c5d9a87bb06cd
Mufaqar Islam <mufaqar@gmail.com>
2022-08-15 18:32:19 +0500

    Catering

---
+++ compnay/temp-catering.php
@@ -0,0 +1,152 @@
+<?php /* Template Name: Catering (C)  */ 
+
+
+
+get_header();
+
+
+?>
+ <?php include('navigation.php'); ?>
+ 
+ <div class="custom_container catering_wrapper mt-5 mb-5">
+                        <!-- <div class="catering_menu">
+                            <a href="">Bread Lunch</a>
+                            <a href="">Salat</a>
+                            <a href="">Vegetarian</a>
+                        </div> -->
+                        <div class="calender_wrapper d-flex justify-content-between align-items-center mt-5">
+                            <div class="catering_heading d-flex align-items-center">
+                                <h2>Catering</h2>
+                                <div><i class="fa-solid fa-plus"></i></div>
+                            </div>
+                            <div class="calender">
+                                <input type="date">
+                            </div>
+                        </div>
+                        <div class="catering_card_wrapper">
+
+                            <div class="catering_card">
+                                <h3>Monday | <span>May 22</span></h3>
+                                <div class="d-flex justify-content-between flex-wrap mt-4">
+                                    <div class="">
+                                        <h6>Number of people:</h6>
+                                        <p>6</p>
+                                    </div>
+                                    <div class="">
+                                        <h6>Address:</h6>
+                                        <p>B-15471-New Jersy</p>
+                                    </div>
+                                    <div class="">
+                                        <h6>Food Type:</h6>
+                                        <p>Wraps</p>
+                                    </div>
+                                    <div class="">
+                                        <h6>Budget per person:</h6>
+                                        <p>NOK 349</p>
+                                    </div>
+                                    <div class="">
+                                        <h6>Need allergens</h6>
+                                        <p>NOK 349</p>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="catering_card">
+                                <h3>Monday | <span>May 22</span></h3>
+                                <div class="d-flex justify-content-between flex-wrap mt-4">
+                                    <div class="">
+                                        <h6>Number of people:</h6>
+                                        <p>6</p>
+                                    </div>
+                                    <div class="">
+                                        <h6>Address:</h6>
+                                        <p>B-15471-New Jersy</p>
+                                    </div>
+                                    <div class="">
+                                        <h6>Food Type:</h6>
+                                        <p>Wraps</p>
+                                    </div>
+                                    <div class="">
+                                        <h6>Budget per person:</h6>
+                                        <p>NOK 349</p>
+                                    </div>
+                                    <div class="">
+                                        <h6>Need allergens</h6>
+                                        <p>NOK 349</p>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="catering_card">
+                                <h3>Monday | <span>May 22</span></h3>
+                                <div class="d-flex justify-content-between flex-wrap mt-4">
+                                    <div class="">
+                                        <h6>Number of people:</h6>
+                                        <p>6</p>
+                                    </div>
+                                    <div class="">
+                                        <h6>Address:</h6>
+                                        <p>B-15471-New Jersy</p>
+                                    </div>
+                                    <div class="">
+                                        <h6>Food Type:</h6>
+                                        <p>Wraps</p>
+                                    </div>
+                                    <div class="">
+                                        <h6>Budget per person:</h6>
+                                        <p>NOK 349</p>
+                                    </div>
+                                    <div class="">
+                                        <h6>Need allergens</h6>
+                                        <p>NOK 349</p>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="catering_card">
+                                <h3>Monday | <span>May 22</span></h3>
+                                <div class="d-flex justify-content-between flex-wrap mt-4">
+                                    <div class="">
+                                        <h6>Number of people:</h6>
+                                        <p>6</p>
+                                    </div>
+                                    <div class="">
+                                        <h6>Address:</h6>
+                                        <p>B-15471-New Jersy</p>
+                                    </div>
+                                    <div class="">
+                                        <h6>Food Type:</h6>
+                                        <p>Wraps</p>
+                                    </div>
+                                    <div class="">
+                                        <h6>Budget per person:</h6>
+                                        <p>NOK 349</p>
+                                    </div>
+                                    <div class="">
+                                        <h6>Need allergens</h6>
+                                        <p>NOK 349</p>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                        </div>
+    
+                    </div>
+                   
+                    
+                </div>
+            </div>
+        </div>
+        
+    </main>
+
+    <?php get_footer();?>
+
+     <!-- Font Awsome -->
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/js/all.min.js" integrity="sha512-6PM0qYu5KExuNcKt5bURAoT6KCThUmHRewN3zUFNaoI6Di7XJPTMoT6K0nsagZKk2OB4L7E3q1uQKHNHd4stIQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+
+
+
+
