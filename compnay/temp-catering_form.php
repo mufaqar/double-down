652ca1958a3d39613dfbf4920fdd01899752cc44
Mufaqar Islam <mufaqar@gmail.com>
2022-08-22 16:00:01 +0500

    catergin

--- compnay/temp-catering_form.php
+++ compnay/temp-catering_form.php
@@ -16,19 +16,7 @@ get_header();
                 <div class="col-md-6 mb-3">
                     <label for="">Number of People</label>
                     <div class="_select">
-                        <select id="people">
-                            <option value="1">01</option>
-                            <option value="2">02</option>
-                            <option value="3">03</option>
-                            <option value="4">04</option>
-                            <option value="5">05</option>
-                            <option value="6">06</option>
-                            <option value="7">07</option>
-                            <option value="8">08</option>
-                            <option value="9">09</option>
-                            <option value="10">10</option>
-                        </select>
-                        <img src="<?php bloginfo('template_directory'); ?>/reources/images/down-arrow.png" alt="">
+                        <input type="text" value="" placeholder="Please enter Number" id="people" required>
                     </div>
                 </div>
 
