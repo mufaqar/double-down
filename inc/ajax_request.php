cd2d338f20b22406c04912e66dbc1568f6853b3d
Mufaqar Islam <mufaqar@gmail.com>
2022-08-25 20:57:24 +0500

    compnay

--- inc/ajax_request.php
+++ inc/ajax_request.php
@@ -186,8 +186,7 @@ add_action('wp_ajax_addmeeting', 'addmeeting', 0);
 add_action('wp_ajax_nopriv_addmeeting', 'addmeeting');
 
 function addmeeting() {		
-	  global $wpdb;		
-	 
+	    global $wpdb;		 
       	$date = $_POST['date'];
 	 	$menu_items = $_POST['menu_items'];
 	 	$uid = $_POST['uid'];      
@@ -245,29 +244,14 @@ add_action('wp_ajax_nopriv_update_deliver_address', 'update_deliver_address');
 
 function update_deliver_address() {		
 	  global $wpdb;		
-
 	  $uid = stripcslashes($_POST['uid']);	
-	  $password = generateRandomString();		
-	  global $wpdb;  
-    //We shall SQL escape all inputs  
-      $address = $_POST['address'];
-      $email = $_POST['username'];    
-	  $password = generateRandomString();	
-	  $user_data = array(
-		'user_login' => $username,
-		'user_email' => $username,
-		'user_pass' => $password,	
-		
-		);
-
-		$user = get_user_by( 'email', $email );
-		$user_id = $user->ID;
-	    $user_id = wp_update_user( array ( 'ID' => $user_id, 'user_pass' => $password ) );	
+	  $address = $_POST['address'];	 
+	    $user_id = update_user_meta( $uid, 'profile_address', $address );
 	  	if (!is_wp_error($user_id)) {		    
-			sendmail($username,$password);
-			echo wp_send_json( array('code' => 200 , 'message'=>__('Password Updated , Please check your email')));
+			//sendmail($username,$password);
+			echo wp_send_json( array('code' => 200 , 'message'=>__('Address Updated')));
 	  	} else {	    		         
-			  echo wp_send_json( array('code' => 0 , 'message'=>__('Error Occured please check your email address')));
+			  echo wp_send_json( array('code' => 0 , 'message'=>__('Error Occured please check address')));
 	      	}
 	  	
 	die;  
