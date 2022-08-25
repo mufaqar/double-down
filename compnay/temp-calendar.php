4a67d24945fed4f0f227162f0f17b4a8b997f3d5
Mufaqar Islam <mufaqar@gmail.com>
2022-08-26 00:20:16 +0500

    updated file

--- compnay/temp-calendar.php
+++ compnay/temp-calendar.php
@@ -6,87 +6,76 @@ get_header('company');
 <!-- tabs -->
 
 <div class="tab_wrapper">
-<?php page_title()?>
+        <?php page_title()?>
         <div class='panels'>
             <div class='panel launchClander'>
-                <h2>Business lunch calendar</h2>
-                <p>Here you can easily choose between or cancel the various lunch options from day
-                    to day. If you want to change a fixed subscription, do so <a href="">her.</a>
-                </p>
+                    <h2>Business lunch calendar</h2>
+                    <p>Here you can easily choose between or cancel the various lunch options from day
+                        to day. If you want to change a fixed subscription, do so <a href="">her.</a>
+                    </p>
 
-          <div class="calender_wrapper d-flex justify-content-between align-items-center">
-                    <div class="calender">
-                        <input type="date">
-                    </div>
-                    <div class="info">
-                        <h6>Total this Week | <span>NOK 0, -</span></h6>                 
-                    </div>
-                </div>
-                <div class="accordion_wrapper">
-                    <div class="row">
-                        <div class="col-lg-12 mx-auto">
-                            <!-- Accordion -->
-                            <div id="accordionExample" class="accordion">
-                                <!-- Accordion item 1 -->
-
-                                <?php
-
-
-                                    $week = [];
-                                    $saturday = strtotime('monday this week');
-                                    $i= 0;
-                                    foreach (range(0, 4) as $day) {
-                                        $week[] = date("Y-m-d", (($day * 86400) + $saturday));
-                                        $today_date =  $week[$i];
-                                        $i++;                                       
-                                        $timestamp = strtotime($today_date);
-                                        $today_day = date('l', $timestamp);
-                                       
-                                     // print_r($week);
-                                        ?>
-
-                                            <div class="card">
-                                                        <form class="dailyfood" id="dailyfood<?php echo $day ?>" action="#">
-                                                            <div id="headingOne" class="card-header bg-white shadow-sm border-0 py-4">
-                                                            <input type="hidden" value="<?php echo $today_date ?>" id="day" >
-                                                            <input type="hidden" value="<?php echo get_current_user_id() ?>" id="uid" >                                                            
-                                                                            <div class="mb-0 d-flex align-items-center">
-                                                                                <button type="button" data-toggle="collapse" data-target="#collapse<?php echo $day?>"
-                                                                                    aria-expanded="true" aria-controls="collapse<?php echo $day?>"
-                                                                                    class="btn text-dark font-weight-bold text-uppercase collapsible-link shadow-none">
-                                                                                    <?php echo $today_day ?> | <span><?php echo $today_date ?></span>
-                                                                                </button>
-                                                                                <h6 class="text-nowrap mb-0"><div class="message">No Booking </div> </h6>
-                                                                            </div>
-                                                                </div>
-                                                                <div id="collapse<?php echo $day?>" aria-labelledby="headingOne" data-parent="#accordionExample"
-                                                                    class="collapse show accordion_content">
-                                                                    <div class="card-body p-md-5">
-                                                                        <?php get_template_part('partials/content', 'daylunch'); ?>
-                                                                    </div>
-                                                            </div>
-                                                        </form>
-                                                </div><!-- End -->
-                                        <?php
-                                    }
-
-                                
-                                    ?>
-
-
-
-
-                              
-
-
-                            </div><!-- End -->
+                   <div class="calender_wrapper d-flex justify-content-between align-items-center">
+                        <div class="calender">
+                            <input type="date">
+                        </div>
+                        <div class="info">
+                            <h6>Total this Week | <span>NOK 0, -</span></h6>                 
                         </div>
                     </div>
-                </div>
-            </div>
+                    <div class="accordion_wrapper">
+                        <div class="row">
+                            <div class="col-lg-12 mx-auto">
+                                <!-- Accordion -->
+                                <div id="accordionExample" class="accordion">
+                                    <?php
+                                        $week = [];
+                                        $saturday = strtotime('monday this week');
+                                        $i= 0;
+                                        foreach (range(0, 4) as $day) {
+                                            $week[] = date("Y-m-d", (($day * 86400) + $saturday));
+                                            $today_date =  $week[$i];
+                                            $i++;                                       
+                                            $timestamp = strtotime($today_date);
+                                            $today_day = date('l', $timestamp);
+                                        
+                                        // print_r($week);
+                                            ?>     <div class="card">
+                                                            <form class="dailyfood" id="dailyfood<?php echo $day ?>" action="#">
+                                                                <div id="headingOne" class="card-header bg-white shadow-sm border-0 py-4">
+                                                                <input type="hidden" value="<?php echo $today_date ?>" id="day" >
+                                                                <input type="hidden" value="<?php echo get_current_user_id() ?>" id="uid" >                                                            
+                                                                                <div class="mb-0 d-flex align-items-center">
+                                                                                    <button type="button" data-toggle="collapse" data-target="#collapse<?php echo $day?>"
+                                                                                        aria-expanded="true" aria-controls="collapse<?php echo $day?>"
+                                                                                        class="btn text-dark font-weight-bold text-uppercase collapsible-link shadow-none">
+                                                                                        <?php echo $today_day ?> | <span><?php echo $today_date ?></span>
+                                                                                    </button>
+                                                                                    <h6 class="text-nowrap mb-0"><div class="message">No Booking </div> </h6>
+                                                                                </div>
+                                                                    </div>
+                                                                    <div id="collapse<?php echo $day?>" aria-labelledby="headingOne" data-parent="#accordionExample"
+                                                                        class="collapse show accordion_content">
+                                                                        <div class="card-body p-md-5">
+                                                                            <?php get_template_part('partials/content', 'daylunch'); ?>
+                                                                        </div>
+                                                                </div>
+                                                            </form>
+                                                    </div><!-- End -->
+                                            <?php
+                                        }                                
+                                        ?>
 
-           
 
+
+
+                                
+
+
+                                </div><!-- End -->
+                            </div>
+                        </div>
+                    </div>
+            </div> 
         </div>
     </div>
 </div>
