d7b54db6f3919a25d0d08a62b3ecb4c5230449f6
syed-muhammad-zunair <zunairgillani54@gmail.com>
2022-08-30 19:06:33 +0500

    sdmin updates

--- admin/temp-foodlist.php
+++ admin/temp-foodlist.php
@@ -1,70 +1,75 @@
-<?php /* Template Name: Admin-Foodlist  */ 
+<?php /* Template Name: Admin-Foodlist  */
 
 
 
 get_header('admin');
 
 
-?>  
-    <?php include('navigation.php'); ?>
-        <div class="custom_container c2 ">
-            <div class="row ">
-                <div class="catering_wrapper mt-5 mb-2 col-md-8">
-                    <div class="catering_menu buttons">
-                        <a id="1" class="showSingle _active" target="1">All Orders</a>
-                        <a id="2" class="showSingle" target="2">Complete</a>
-                        <a id="3" class="showSingle" target="3">Pending</a>
-                        <a id="4" class="showSingle" target="4">Cancle</a>
-                    </div>
+?>
+<?php include('navigation.php'); ?>
+<div class="admin_parrent _admin_foodlist">
+    <div class="toggle_btn">
+        <div class="row ">
+            <div class="catering_wrapper mt-5 mb-2  p-0 w-100">
+                <div class="catering_menu buttons">
+                    <a id="1" class="showSingle _active" target="1">All Orders</a>
+                    <a id="2" class="showSingle" target="2">Complete</a>
+                    <a id="3" class="showSingle" target="3">Pending</a>
+                    <a id="4" class="showSingle" target="4">Cancle</a>
                 </div>
             </div>
         </div>
-        <section id="div1" class="targetDiv activediv tablediv">
-            <table id="all" class="table table-striped orders_table" style="width:100%">
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Order ID</th>
-                        <th>Order Type</th>
-                        <th>User Type</th>
-                        <th>Date</th>
-                        <th>Price</th>
-                        <th>Status</th>
-                    </tr>
-                </thead>
-                <tbody>
+    </div>
 
-                <?php 
+    <section id="div1" class="targetDiv activediv tablediv">
+        <table id="all" class="table table-striped orders_table" style="width:100%">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Product Name</th>
+                    <th>Product Category</th>
+                    <th>Product Sub-Categories</th>
+                    <th>Date</th>
+                    <th>Price</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+
+                <?php
                 $i = 0;
-                    
-                    query_posts(array(
-                            'post_type' => 'orders',
-                            'posts_per_page' => -1,
-                            'order' => 'desc',
-                            
-                            
-                        ));              
-                
-                        if (have_posts()) :  while (have_posts()) : the_post(); $i++; ?>
-                            <tr>
-                                <td><?php echo $i ?></td>
-                                <td><?php the_title()?></td>
-                                <td><?php echo get_post_meta( get_the_ID(), 'order_type', true ); ?></td>
-                                <td><?php echo get_post_meta( get_the_ID(), 'user_type', true ); ?></td>
-                                <td><?php echo get_post_meta( get_the_ID(), 'date', true ); ?></td>
-                                <td>NOK <?php echo get_post_meta( get_the_ID(), 'total_price', true ); ?></td>
-                                <td> <span class="status"><?php echo get_post_meta( get_the_ID(), 'order_status', true ); ?> </span> </td>
-                            </tr>
-                        <?php endwhile; wp_reset_query(); else : ?>
-                            <h2><?php _e('Nothing Found','lbt_translate'); ?></h2>
-                        <?php endif; ?>  
 
-                    </tbody>
+                query_posts(array(
+                    'post_type' => 'orders',
+                    'posts_per_page' => -1,
+                    'order' => 'desc',
 
-                </table>
-                
-            </section>
 
+                ));
+
+                if (have_posts()) :  while (have_posts()) : the_post();
+                        $i++; ?>
+                        <tr>
+                            <td><?php echo $i ?></td>
+                            <td><?php the_title() ?></td>
+                            <td><?php echo get_post_meta(get_the_ID(), 'order_type', true); ?></td>
+                            <td><?php echo get_post_meta(get_the_ID(), 'user_type', true); ?></td>
+                            <td><?php echo get_post_meta(get_the_ID(), 'date', true); ?></td>
+                            <td>NOK <?php echo get_post_meta(get_the_ID(), 'total_price', true); ?></td>
+                            <td> <img class="p_image" src="http://demo.mufaqar.com/dev3/wp-content/uploads/2022/08/rest3.jpg" alt="product_image" /> </td>
+                        </tr>
+                    <?php endwhile;
+                    wp_reset_query();
+                else : ?>
+                    <h2><?php _e('Nothing Found', 'lbt_translate'); ?></h2>
+                <?php endif; ?>
+
+            </tbody>
+
+        </table>
+
+    </section>
+</div>
 
 
 </div>
@@ -75,7 +80,3 @@ get_header('admin');
 </main>
 
 <?php get_footer('admin') ?>
-
-
-
-
--- admin/temp-users.php
+++ admin/temp-users.php
@@ -1,69 +1,72 @@
-<?php /* Template Name: Admin-Users  */ 
+<?php /* Template Name: Admin-Users  */
 
 
 
 get_header('admin');
 
 
-?>  
-    <?php include('navigation.php'); ?>
-        <div class="custom_container c2 ">
-            <div class="row ">
-                <div class="catering_wrapper mt-5 mb-2 col-md-8">
-                    <div class="catering_menu buttons">
-                        <a id="1" class="showSingle _active" target="1">All Users</a>
-                        <a id="2" class="showSingle" target="2">All Companites</a>
-                    </div>
+?>
+<?php include('navigation.php'); ?>
+<div class="admin_parrent">
+    <div class="toggle_btn">
+        <div class="row ">
+            <div class="catering_wrapper mt-5 mb-2 col-md-8 p-0">
+                <div class="catering_menu buttons">
+                    <a id="1" class="showSingle _active" target="1">All Users</a>
+                    <a id="2" class="showSingle" target="2">All Companites</a>
                 </div>
             </div>
         </div>
-        <section id="div1" class="targetDiv activediv tablediv">
-            <table id="all" class="table table-striped orders_table" style="width:100%">
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Name</th>                     
-                        <th>Compnay</th>
-                        <th>Phone Number</th>
-                        <th>Email Address</th>
-                        <th>...</th>
-                    </tr>
-                </thead>
-                <tbody>
+    </div>
+    <section id="div1" class="targetDiv activediv tablediv">
+        <table id="all" class="table table-striped orders_table" style="width:100%">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Name</th>
+                    <th>Compnay</th>
+                    <th>Phone Number</th>
+                    <th>Email Address</th>
+                    <th>...</th>
+                </tr>
+            </thead>
+            <tbody>
 
-                <?php 
+                <?php
                 $i = 0;
-                    
+
                 $members = get_users(
                     array(
                         //'role'    => 'company',
                         'orderby' => 'ID',
                         'order'   => 'ASC'
                     )
-                );    
-                $users = get_users( $members );   
-              //  print "<pre>";
-              //  print_r($users); 
-                
-             
-                
-                foreach ( $users as $user ) { $i++;  ?>
-                            <tr>                            
-                                <td><?php echo $i ?></td>  
-                                <td><?php echo $user->display_name ?></td>
-                                <td><?php echo get_user_meta($user->ID , 'compnay_name' , true);?></td>
-                                <td><?php echo get_user_meta($user->ID , 'profile_delivery_phone' , true);?></td>                             
-                                <td><?php echo $user->user_email ?></td>
-                                <th>...</th>
-                    
-                            </tr>
-                        <?php } ?>  
+                );
+                $users = get_users($members);
+                //  print "<pre>";
+                //  print_r($users); 
 
-                    </tbody>
 
-                </table>
-                
-            </section>
+
+                foreach ($users as $user) {
+                    $i++;  ?>
+                    <tr>
+                        <td class="pt-4"><?php echo $i ?></td>
+                        <td class="d-flex align-items-center"><img class="_user_profile" src="http://demo.mufaqar.com/dev3/wp-content/uploads/2022/08/Ellipse-103.png" alt="profile" /><?php echo $user->display_name ?></td>
+                        <td><?php echo get_user_meta($user->ID, 'compnay_name', true); ?></td>
+                        <td><?php echo get_user_meta($user->ID, 'profile_delivery_phone', true); ?></td>
+                        <td><?php echo $user->user_email ?></td>
+                        <th>...</th>
+
+                    </tr>
+                <?php } ?>
+
+            </tbody>
+
+        </table>
+
+    </section>
+</div>
 
 
 
@@ -75,7 +78,3 @@ get_header('admin');
 </main>
 
 <?php get_footer('admin') ?>
-
-
-
-
