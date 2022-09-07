9ee77bacbb16f4d8ed9b8e485f785915e78b0d81
Mufaqar Islam <mufaqar@gmail.com>
2022-09-07 20:11:01 +0500

    date

--- partials/menu-lunch.php
+++ partials/menu-lunch.php
@@ -15,20 +15,15 @@
         ));
         if (have_posts()) :  while (have_posts()) : the_post(); ?>
                 <div class="catering_card _pro_salat">
-                    <h3><?php the_title() ?> ( Monday | <span>May 22</span> ) </h3>
+                    <h3><?php the_title() ?> ( Monday | <span><?php echo get_post_meta( get_the_ID(), 'available_date', true ); ?></span> ) </h3>
                     <p class="mt-3"><?php the_content() ?></p>
                     <div class="row">
-                        <div class="col-md-9">
+                        <div class="col-md-12">
                             <h6 class="mt-2">Contains:</h6>
                             <p><?php echo get_post_meta(get_the_ID(), 'contains', true); ?></p>
                             <p>Nutritional content: <?php echo get_post_meta(get_the_ID(), 'nutritional_content', true); ?></p>
                         </div>
-                        <div class="col-md-3 level d-flex justify-content-end">
-                            <div class="d-flex justify-content-center align-items-center flex-column ">
-                                <img src="<?php echo get_template_directory_uri(); ?>/reources/images/level.png" alt="level">
-                                <h6 class="mt-2"><?php echo get_post_meta(get_the_ID(), 'calories', true); ?> <sub> kg CO e</sub></h6>
-                            </div>
-                        </div>
+                        
                     </div>
                 </div>
             <?php endwhile;
@@ -38,35 +33,5 @@
         <?php endif; ?>
     </div>
 
-    <div class="mt-5 bottom_level d-flex justify-content-between align-items-center">
-
-        <div class="d-flex align-items-center">
-            <img src="<?php echo get_template_directory_uri(); ?>/reources/images//level.png" alt="">
-            <div>
-                <h6>Lav</h6>
-                <p>0.1-0.5</p>
-                <p>kg CO2<sub>e</sub></p>
-            </div>
-        </div>
-
-        <div class="d-flex align-items-center">
-            <img src="<?php echo get_template_directory_uri(); ?>/reources/images//level.png" alt="">
-            <div>
-                <h6>Medium</h6>
-                <p>0.1-0.5</p>
-                <p>kg CO2<sub>e</sub></p>
-            </div>
-        </div>
-
-        <div class="d-flex align-items-center">
-            <img src="<?php echo get_template_directory_uri(); ?>/reources/images//level.png" alt="">
-            <div>
-                <h6>Hight</h6>
-                <p>0.1-0.5</p>
-                <p>kg CO2<sub>e</sub></p>
-            </div>
-        </div>
-
-    </div>
 
 </div>
