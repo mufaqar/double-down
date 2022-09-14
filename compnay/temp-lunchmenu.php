1d5c362add9f32d5fd4b4a2dc1de4767f1b92550
Mufaqar Islam <mufaqar@gmail.com>
2022-09-14 22:03:17 +0500

    compnay menu

--- compnay/temp-lunchmenu.php
+++ compnay/temp-lunchmenu.php
@@ -1,39 +1,71 @@
 <?php /* Template Name: Company-LunchMenu  */
-get_header('company');
-?>
-<?php include('navigation.php'); ?>
-<div class="tab_wrapper">
-    <?php page_title() ?>
-</div>
+get_header('compnay');
 
-<div class="custom_container c2 ">
-    <div class="row ">
-        <div class="catering_wrapper mt-5 mb-5 col-md-8">
-            <div class="catering_menu buttons">
-                <a id="1" class="showSingle _active" target="1"> Bread Lunch</a>
-                <a id="2" class="showSingle" target="2">Salad Lunch</a>
-                <a id="3" class="showSingle" target="3">Wrap Lunch</a>
+$no_week =  date("Y-W");
+$query_week = $_REQUEST['week'];
+if($query_week == '') {
+    $query_week = $no_week;
+}
+
+$week_arr = explode("-", $query_week, 2);
+$week=$week_arr[1];
+$year=$week_arr[0];
+?>
+    <?php include('navigation.php'); ?>
+    <div class="tab_wrapper">
+        <?php page_title() ?>
+    </div>
+
+    <div class="custom_container c2 ">
+        <div class="row ">
+            <div class="catering_wrapper mt-5 mb-5 col-md-8">
+                <div class="catering_menu buttons">
+                    <a id="1" class="showSingle _active" target="1" data="bread-lunch"> Bread Lunch</a>
+                    <a id="2" class="showSingle" target="2" data="salad-lunch">Salad Lunch</a>
+                    <a id="3" class="showSingle" target="3" data="wrap-lunch">Wrap Lunch</a>
+                </div>
             </div>
         </div>
     </div>
-</div>
 
 
-<section id="div1" class="targetDiv activediv">
-    <?php get_template_part('partials/menu', 'lunch'); ?>
-</section>
-<section id="div2" class="targetDiv">
-    <?php get_template_part('partials/menu', 'salad'); ?>
-</section>
-<section id="div3" class="targetDiv">
-    <?php get_template_part('partials/menu', 'wrap'); ?>
-</section>
+    <div class="custom_container catering_wrapper ">                       
+                       <div class="calender_wrapper d-flex justify-content-between align-items-center mt-5">
+                            <h2>This week's <span id="type">Bread Lunch</span> menu</h2>
+                            <div class="calender week_calender">
+                                    <form action="" method="GET" id="weekform">
+                                        <input type="hidden" name="catname" id="catname" value="bread-lunch" />
+                                        <input type="text" id="weekPicker1" name="week" value="<?php echo date("Y-W"); ?>" >
+                                        <div class="wc-icon"><i class="fa-solid fa-calendar-days"></i></div>
+                                    </form>
+                            </div> 
+                       </div>
+    
+    <section id="div1" class="targetDiv activediv">
 
-</div>
-</div>
-</div>
+    <?php  get_template_part( 'partials/menu', 'lunch' );  ?>
+          
+    </section>
+                
+    <section id="div2" class="targetDiv">
 
-</main>
+    <?php  get_template_part( 'partials/menu', 'salad' );  ?>
+
+
+    
+    </section>
+    <section id="div3" class="targetDiv">
+        <?php  get_template_part( 'partials/menu', 'wrap' );  ?>
+    
+    </section>
+
+    </div>
+
+    </div>
+    </div>
+    </div>
+
+    </main>
 
 
 <?php get_footer(); ?>
@@ -62,46 +94,72 @@ get_header('company');
 <script src="http://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
 <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/reources/js/weekPicker.min.js"></script>
 <script>
-    convertToWeekPicker($("#date"));
+    convertToWeekPicker($("#weekPicker1"));
     convertToWeekPicker($("#weekPicker2"));
     convertToWeekPicker($("#weekPicker3"));
+    window.addEventListener('load', function() {
+            var element = document.getElementById('displayDate');
+            var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
+            var observer = new MutationObserver(myFunction);
+            observer.observe(element, {
+                childList: true
+            });
+            function myFunction() {   
+                       
+               // document.getElementById("weekform").submit();   
+               jqueryFunction();        
+                }
+            
+        });
+
 </script>
 
 <script>  
 
+
+
+
 jQuery(document).ready(function($) 
         {   
 
+            jqueryFunction = function(  )
+                    {
+                        var weekid = jQuery('#weekPicker1').val(); 
+                        var catname = jQuery('#catname').val(); 
 
-        var startDate,
-        endDate;
-        
-      $('#weekpicker').datepicker({
-        autoclose: true,
-        format :'mm/dd/yyyy',
-        forceParse :false
-    }).on("changeDate", function(e) {
-        alert("sasdfasfd");
-        //console.log(e.date);
-        var date = e.date;
-        startDate = new Date(date.getFullYear(), date.getMonth(), date.getDate() - date.getDay());
-        endDate = new Date(date.getFullYear(), date.getMonth(), date.getDate() - date.getDay()+6);
-        //$('#weekpicker').datepicker("setDate", startDate);
-        $('#weekpicker').datepicker('update', startDate);
-        $('#weekpicker').val((startDate.getMonth() + 1) + '/' + startDate.getDate() + '/' +  startDate.getFullYear() + ' - ' + (endDate.getMonth() + 1) + '/' + endDate.getDate() + '/' +  endDate.getFullYear());
-    });
-        
+                        
+                        alert(catname); 
+                          
+                        $.ajax({
+                            type:"POST",
+                            url: "<?php echo admin_url('admin-ajax.php'); ?>",
+                            data: {
+                                action: "get_type_products",
+                                weekid : weekid,
+                                catname : catname
+
+                            },           
+                            success: function(data) {
+                                if (data.code == 0) {
+                                    alert(data.message);
+                                } else {  
+                                    
+                                   // alert("Ajax Working");
+                                  $(".foodlist").html(data);
+                                  $(".oldfoodlist").hide();
+                                    
+                                }
+                            }
+
+                        });
+                    }
+
+            
 
 
-            $('#date').on('dp.change', function (e) { 
-                alert("asdfasdf");
-                });
 
 
-           
-
-
-
+       
 
 
         jQuery(function() {  
@@ -110,6 +168,9 @@ jQuery(document).ready(function($)
         jQuery('.showSingle').click(function() {
             $(".showSingle").removeClass("_active");
             $(this).addClass("_active");
+          //  $('#weekform').append('<input type="text" name="catname" id="catname" value='+$(this).attr('target')+' />');
+            $("#catname").val($(this).attr('data') );
+            $("span#type").html($(this).attr('data'));            
             jQuery('.targetDiv').hide();
             jQuery('#div' + $(this).attr('target')).show();
         });
