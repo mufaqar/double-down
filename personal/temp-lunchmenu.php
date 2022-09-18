aa3d90aea73290a4cb839477c63a2b7b22c955f6
Mufaqar Islam <mufaqar@gmail.com>
2022-09-18 12:45:20 +0500

    lunch

--- personal/temp-lunchmenu.php
+++ personal/temp-lunchmenu.php
@@ -31,7 +31,7 @@ $year=$week_arr[0];
 
     <div class="custom_container catering_wrapper ">                       
                        <div class="calender_wrapper d-flex justify-content-between align-items-center mt-5">
-                            <h2>This week's <span id="type">Bread Lunch</span> menu</h2>
+                            <h2>This week's <span id="type">bread lunch</span> menu</h2>
                             <div class="calender week_calender">
                                     <form action="" method="GET" id="weekform">
                                         <input type="hidden" name="catname" id="catname" value="bread-lunch" />
