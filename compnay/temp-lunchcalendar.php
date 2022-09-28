c9a3576da99198666deb08cca44cd00cabdac5bf
Mufaqar Islam <mufaqar@gmail.com>
2022-09-28 17:52:36 +0500

    lunh calendar

--- compnay/temp-lunchcalendar.php
+++ compnay/temp-lunchcalendar.php
@@ -139,10 +139,12 @@ global $current_user; wp_get_current_user();  $uid = $current_user->ID;
                     if (have_posts()) :  while (have_posts()) : the_post(); $new_id = get_the_ID().$i; ?>
                             <div class="col-md-6 first border-end mb-5">
                                 <h3><?php the_title() ?> | <span> NOK <?php the_field('menu_item_price'); ?></span></h3>
-                                <p>Here you can easily choose between or cancel
-                                    the various lunch options from day to day.
-                                    If you want to change a fixed subscription,
-                                    do so <a href="<?php echo home_url('contact-us'); ?>">her.</a></p>
+                                <p class="mt-3"><?php the_content() ?></p>
+                                <h6 class="mt-2">Allergenes:</h6>
+                                    <?php $allergense =  get_the_terms( $post->ID, 'menu_sub_types' );                                     
+                                    foreach($allergense as $allergy) {
+                                        echo "<p>".$allergy->name ."</p>";
+                                    } ?> 
                                 <div class="order_wrapper mt-3">                                 
                                     <button class="btn_primary _id<?php echo $new_id ?>" onmouseover="showOrderCounter(<?php echo $new_id?>)">Select</button>
                                     <div class="d-none product_counter  d-flex justify-content-center align-items-center _cid<?php echo $new_id; ?>">
