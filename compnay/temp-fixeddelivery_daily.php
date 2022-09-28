f01778562df855e0474dfc88657a0cc596c9af83
Mufaqar Islam <mufaqar@gmail.com>
2022-09-28 14:33:53 +0500

    updated lunch boxed

--- compnay/temp-fixeddelivery_daily.php
+++ compnay/temp-fixeddelivery_daily.php
@@ -61,39 +61,7 @@ get_header('company');
                     <input type="hidden" value="<?php echo get_current_user_id() ?>" id="uid">
                     <input type="hidden" value="Company" id="usertype">
                     <input type="hidden" value="<?php echo date("Y-m-d"); ?>" id="tdate">
-
                     <?php get_template_part( 'partials/lunchbox', 'fixeditems' ); ?>
-
-
-                    <?php query_posts(array(
-                        'post_type' => 'menu_items',
-                        'posts_per_page' => -1,
-                        'order' => 'desc',
-                        'menu_types' => 'lunch-boxes'
-                    ));
-                    if (have_posts()) :  while (have_posts()) : the_post();
-                            $pid = get_the_ID(); ?>
-                            <div class="col-md-6 col-lg-4 mt-4">
-                                <div class="product_card p-4">
-                                <?php if ( has_post_thumbnail() ) {
-                                                               the_post_thumbnail('product-thumbnail' , array( 'class'  => 'pro_img' ));
-                                                            } else { ?>
-                                                        <img src="<?php echo get_template_directory_uri(); ?>/reources/images/product1.png" alt="Featured Thumbnail" />
-                                                        <?php } ?>
-                                    <h2><?php the_title(); ?> , NOK <?php the_field('menu_item_price'); ?> </h2>
-                                    <button id="<?php echo $pid; ?>" class="btn_primary  select_product_btn id<?php echo $pid; ?>" onmouseover="showCounter(<?php echo $pid; ?>)">Select</button>
-                                    <div class="d-none product_counter mt-2 d-flex justify-content-center align-items-center cid<?php echo $pid; ?>">
-                                        <i class="count-down" onclick="handleCountDec(<?php echo $pid ?>)"><img src="<?php echo get_template_directory_uri(); ?>/reources/images/neg.png" alt=""></i>
-                                        <input type="text" data-id="<?php echo $pid; ?>" value="1" class="product-quantity form-control text-center incrDecrCounter" />
-                                        <i class="count-up" onclick="handleCountInc(<?php echo $pid ?>)"><img src="<?php echo get_template_directory_uri(); ?>/reources/images/plus.png" alt=""></i>
-                                    </div>
-                                </div>
-                            </div>
-                        <?php endwhile;
-                        wp_reset_query();
-                    else : ?>
-                        <h2><?php _e('Nothing Found', 'lbt_translate'); ?></h2>
-                    <?php endif; ?>
                 </div>
 
                 <div class="d-flex justify-content-between mt-5 mb-4 accessories">
