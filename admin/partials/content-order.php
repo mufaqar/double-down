0d3146fadeba4fc8bfd2830a2d6fafd3f437a829
Mufaqar Islam <mufaqar@gmail.com>
2022-10-10 16:58:11 +0500

    admin roders

--- admin/partials/content-order.php
+++ admin/partials/content-order.php
@@ -2,10 +2,10 @@
                         <tr>
                             <th>Sr #</th>
                             <th>Order ID</th>
-                            <th>Order Type</th>
-                            <th>User Type</th>
-                             <th>Order Date</th>
-                             
+                            <th>Date</th>
+                            <th>Name</th>
+                            <th>Products</th>  
+                            <th>Allergenes</th>
                             <th>Price</th>
                             <th>Status</th>
                         </tr>
@@ -14,25 +14,65 @@
 
                     <?php 
                         $i = 0;
-
                         query_posts(array(
                             'post_type' => 'orders',
                             'posts_per_page' => -1,
                             'order' => 'desc',
-
-
                         ));
 
                         if (have_posts()) :  while (have_posts()) : the_post();
+
+                        $order_uid = get_post_meta(get_the_ID(), 'order_uid', true);
+                        $user_info = get_userdata($order_uid);
+                 
+                        $food_items =  get_post_meta( get_the_ID(), 'food_order', true );		
+                       
+                      
                                 $i++; ?>
                                 <tr>
                                     <td><?php echo $i ?></td>
                                     <td><?php the_title() ?></td>
-                                    <td><?php echo get_post_meta(get_the_ID(), 'order_type', true); ?></td>
-                                    <td><?php echo get_post_meta(get_the_ID(), 'user_type', true); ?></td>       
-                                      
-                                    <td><?php the_date('Y-m-d');?></td>                          
-                                    <td>NOK <?php echo get_post_meta(get_the_ID(), 'total_price', true); ?></td>
+                                    <td><?php the_date('Y-m-d');?></td>    
+                                    <td><?php echo $user_info->user_login ?></td>  
+                                    <td>
+                                        <ul>
+
+                                    <?php
+
+                                    foreach($food_items as $index => $food){
+                                        ?>
+
+                                            <li>
+                                                <h5><strong><?php echo $index ?></h5>
+                                              
+                                              
+                                                <?php   foreach($food as $key => $ky_item) { 	?>
+                                                    <p>
+                                                        <?php echo  get_the_title($key) . " [". $ky_item . "] " ; ?>
+                                                       
+                                                        <p>
+                                                
+                                                    <?php 	}  ?>
+                                                   
+                                               
+                                          
+                                                   
+                                             
+																
+                                                </li>
+
+                                        <?php
+
+
+                                    }
+
+
+
+                                    ?> </ul>
+                                    </td>  
+                                    <td></td>   
+                                                         
+                                    <td>NOK <?php echo get_post_meta(get_the_ID(), 'order_total', true); ?></td>
                                     <td><span class="status <?php echo get_post_meta(get_the_ID(), 'order_status', true); ?>"><?php echo get_post_meta(get_the_ID(), 'order_status', true); ?> </span> </td>
                                 </tr>
                             <?php endwhile;
