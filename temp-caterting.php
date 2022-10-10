609423a2192cfaee4b35a4fd518862ee124148d8
Mufaqar Islam <mufaqar@gmail.com>
2022-10-10 16:02:42 +0500

    catering

--- temp-caterting.php
+++ temp-caterting.php
@@ -22,28 +22,10 @@ $year=$week_arr[0];
                     </div>
                     <div>
                         
-                        <?php 	
-								  wp_nav_menu ( array(
-                                    'container'       => false,	
-									'theme_location'  => 'profile',	
-									'menu_class'      => 'myProfileNav activeNav'
-								) );
-								?>
-
                        
-                            
-                        <?php 	
-								  wp_nav_menu ( array(
-									'theme_location'  => 'company',								
-									'container'       => false,									
-									'menu_class'      => 'companyProfileNav'
-								) );
-								?>
 
                     </div>
-                    <div class="logout">                       	
-                    <a href="<?php echo wp_logout_url( home_url() ); ?>"> <img src="<?php bloginfo('template_directory'); ?>/reources//images/logout.png" alt=""><span>Log Out</span></a>
-                    </div>
+                   
                     <img src="<?php bloginfo('template_directory'); ?>/reources/images/cancel.png" class="hide_nav" alt="" onclick="HideNav()">
             </div>
         </div>    
