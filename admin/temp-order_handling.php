c9eb8c2ebb7baf99848234012539212e8ab2097d
Mufaqar Islam <mufaqar@gmail.com>
2022-10-10 18:25:11 +0500

    order data

--- admin/temp-order_handling.php
+++ admin/temp-order_handling.php
@@ -22,7 +22,61 @@
             </div>
             <section id="div1" class="targetDiv activediv tablediv">
                 <table id="all" class="table table-striped orders_table" style="width:100%">
-                <?php include('partials/content-order_handling.php'); ?>
+                <thead>
+                        <tr>
+                            <th>Sr #</th>
+                            <th>Order ID</th>
+                            <th>Company Name</th>
+                            <th>Address</th>                          
+                            <th>Price</th>
+                            <th>PDF</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                    <?php 
+                        $i = 0;
+                        query_posts(array(
+                            'post_type' => 'orders',
+                            'posts_per_page' => -1,
+                            'order' => 'desc',
+                        ));
+
+                        if (have_posts()) :  while (have_posts()) : the_post();
+
+                        $order_uid = get_post_meta(get_the_ID(), 'order_uid', true);
+                        $user_info = get_userdata($order_uid);
+                 
+                        $food_items =  get_post_meta( get_the_ID(), 'food_order', true );	
+                        $address =  get_user_meta( $order_uid, 'compnay_delivery_address', true );	
+                        $company_name =  get_user_meta( $order_uid, 'compnay_name', true );	
+                        
+                       
+                      
+                                $i++; ?>
+                                <tr>
+                                    <td><?php echo $i ?></td>
+                                    <td><?php the_title() ?></td>
+                                    <td><?php echo $company_name ?></td>    
+                                    <td><?php echo $address?></td>  
+                                    
+                                    
+                                                         
+                                    <td>NOK <?php echo get_post_meta(get_the_ID(), 'order_total', true); ?></td>
+                                  
+                                    <td><span class="status <?php echo get_post_meta(get_the_ID(), 'order_status', true); ?>"><?php echo get_post_meta(get_the_ID(), 'order_status', true); ?> </span> </td>
+                                    <td> PDF</td>   
+                               
+                                </tr>
+                            <?php endwhile;
+                            wp_reset_query();
+                        else : ?>
+                            <h2><?php _e('Nothing Found', 'lbt_translate'); ?></h2>
+                        <?php endif; ?>
+
+                    </tbody>
+
                 </table>
 
             </section>
