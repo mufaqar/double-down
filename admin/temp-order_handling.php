9243c91fb9278faf2363f763d6ea3871c47d3b61
Mufaqar Islam <mufaqar@gmail.com>
2022-10-14 19:16:26 +0500

    updated orders

--- admin/temp-order_handling.php
+++ admin/temp-order_handling.php
@@ -5,81 +5,82 @@
     get_header('admin');
 
 
+
     ?>
-        <?php include('navigation.php'); ?>
-        <div class="admin_parrent">
-            <div class="toggle_btn">
-                <div class="row ">
-                    <div class="catering_wrapper mt-5 mb-2  p-0 w-100">
-                        <div class="catering_menu buttons">
-                        <a id="1" class="showSingle _active" target="1" data="">All</a>
-                        <a id="2" class="showSingle" target="2" data="Complete">Complete</a>
-                        <a id="3" class="showSingle" target="3" data="Pending">Pending</a>
-                        <a id="4" class="showSingle" target="4" data="Cancel">Cancel</a>
-                        </div>
+    <?php include('navigation.php'); ?>
+    <div class="admin_parrent">
+        <div class="toggle_btn">
+            <div class="row ">
+                <div class="catering_wrapper mt-5 mb-2 col-md-8 p-0">
+                    <div class="catering_menu buttons">
+                        <a id="1" class="showSingle _active" target="1" data="">All Users</a>
+                        <a id="2" class="showSingle" target="2" data="personal">Personal Profile</a>
+                        <a id="2" class="showSingle" target="2" data="Company">All Companies</a>
                     </div>
                 </div>
             </div>
-            <section id="div1" class="targetDiv activediv tablediv">
-                <table id="all" class="table table-striped orders_table" style="width:100%">
-                <thead>
-                        <tr>
-                            <th>Sr #</th>
-                            <th>Order ID</th>
-                            <th>Company Name</th>
-                            <th>Address</th>                          
-                            <th>Price</th>
-                           
-                            <th>Status</th>
-                            <th>PDF</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-
-                    <?php 
-                        $i = 0;
-                        query_posts(array(
-                            'post_type' => 'orders',
-                            'posts_per_page' => -1,
-                            'order' => 'desc',
-                        ));
-
-                        if (have_posts()) :  while (have_posts()) : the_post();
-                                $order_uid = get_post_meta(get_the_ID(), 'order_uid', true);
-                                $user_info = get_userdata($order_uid);                 
-                                $food_items =  get_post_meta( get_the_ID(), 'food_order', true );	
-                                $address =  get_user_meta( $order_uid, 'compnay_delivery_address', true );	
-                                $company_name =  get_user_meta( $order_uid, 'compnay_name', true );
-                                $i++; ?>
-                                <tr>
-                                    <td><?php echo $i ?></td>
-                                    <td><?php the_title() ?></td>
-                                    <td><?php echo $company_name ?></td>    
-                                    <td><?php echo $address?></td>  
-                                    
-                                    
-                                                         
-                                    <td>NOK <?php echo get_post_meta(get_the_ID(), 'order_total', true); ?></td>
-                                  
-                                    <td><span class="status <?php echo get_post_meta(get_the_ID(), 'order_status', true); ?>"><?php echo get_post_meta(get_the_ID(), 'order_status', true); ?> </span> </td>
-                                    <td> View </td>   
-                               
-                                </tr>
-                            <?php endwhile;
-                            wp_reset_query();
-                        else : ?>
-                            <h2><?php _e('Nothing Found', 'lbt_translate'); ?></h2>
-                        <?php endif; ?>
-
-                    </tbody>
-
-                </table>
-
-            </section>
-
         </div>
+        <section id="div1" class="targetDiv activediv tablediv">
+            <table id="allusers" class="table table-striped orders_table" style="width:100%">
+                <thead>
+                    <tr>
+                        <th>Sr #</th>
+                        <th>Name</th>
+                        <th>Type</th>                     
+                        <th>Email Address</th>
+                        <th>Orders</th>
+                       
+                    </tr>
+                </thead>
+                <tbody>    
+                    <?php
+                    $i = 0;
+    
+                    $members = get_users(
+                        array(    
+                            'role' => 'Company',                  
+                            'orderby' => 'ID',
+                            'order'   => 'ASC',
+                            'query_id' => 'authors_with_posts',
+                        
+                        )
+                    );
+                    $users = get_users($members);               
+    
+                    foreach ($users as $user) {
+                         $user_roles = $user->roles;    
+                         $comapnay_name = get_user_meta($user->ID, 'compnay_name', true);                       
+                        $i++; 
+                        $user_orders = count_user_posts($user->ID ,"orders");
 
+                        if(count_user_posts($user->ID,'orders') >= 1)
+                        {
+	
 
-
-   
-<?php get_footer('admin') ?>
+                        
+                        ?>
+                        <tr>
+                            <td class="pt-4"><?php echo $i ?></td>
+                            <td class="d-flex align-items-center"><img class="_user_profile" src="<?php echo esc_url( get_avatar_url( $user->ID ) ); ?>" alt="profile" />
+                            <?php echo $user->display_name ;   ?></td>
+                            <td><?php echo ucfirst($user_roles[0]); if($comapnay_name != '') { echo " [". $comapnay_name ." ]" ;} ?></td>
+                             <td><?php echo $user->user_email ?></td>
+                             <th>    <button id="show_user_orders" class="btn_primary">View Orders [<?php echo $user_orders ?>]</button></th>
+                          
+    
+                        </tr>
+                    <?php  }  } ?>
+    
+                </tbody>
+    
+            </table>
+    
+        </section>
+        
+    </div>
+    
+    
+    
+    
+    
+    <?php get_footer('admin') ?>
