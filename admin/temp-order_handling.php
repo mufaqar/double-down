d6ccb495c750953ef31e1e696d68549da21fc0c8
Mufaqar Islam <mufaqar@gmail.com>
2022-10-20 19:40:23 +0500

    udpated

--- admin/temp-order_handling.php
+++ admin/temp-order_handling.php
@@ -79,16 +79,7 @@
     </section>
 
 
-    
-    
-    <?php get_footer('admin') ?>
-
-  
-    
-
-
-
-<script src="http://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
+    <script src="http://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
 <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/reources/js/weekPicker.min.js"></script>
 <script>
     convertToWeekPicker($("#weekPicker1"));
@@ -109,10 +100,19 @@
         });
 
 </script>
-
+    
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     
+
+    
+    <?php get_footer('admin') ?>
+
+  
+    
+
+
+
 
 <script type="text/javascript">
 
