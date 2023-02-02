a6a07457acbbc0fea6e33f0381b0882b20c101eb
Mufaqar Islam <mufaqar@gmail.com>
2023-02-02 16:48:08 +0500

    text domain

--- compnay/temp-blog.php
+++ compnay/temp-blog.php
@@ -28,7 +28,7 @@ get_header('company'); ?>
                                     </div>   
                         <?php endwhile; ?>        
                     <?php else : ?>
-                        <p class="noposts"><?php _e('To add Blog Posts, go to Admin Panel > Posts > Add New','author_ts'); ?></p>
+                        <p class="noposts"><?php _e('To add Blog Posts, go to Admin Panel > Posts > Add New','text_domain'); ?></p>
                     <?php endif; ?>
                 </div>
                 <div class="d-none load_more d-flex justify-content-center mt-5 mb-5">
