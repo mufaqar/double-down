25929dcb9f2d886cc89756f11cd9fe4d6aaeeb4f
Mufaqar Islam <mufaqar@gmail.com>
2023-02-02 17:11:34 +0500

    week days

--- personal/temp-lunchcalendar.php
+++ personal/temp-lunchcalendar.php
@@ -515,6 +515,16 @@ jQuery(document).ready(function($)
        $('#date').change(function() {
       $(this).closest('form').submit();
        }); 
+
+       const picker = document.getElementById('date');
+picker.addEventListener('input', function(e){
+  var day = new Date(this.value).getUTCDay();
+  if([6,0].includes(day)){
+    e.preventDefault();
+    this.value = '';
+    alert('Weekends not allowed');
+  }
+});
        
        
        $('#date-datepicker div').datepicker({
