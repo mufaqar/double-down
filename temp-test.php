5784c017fb30e7e1f8aea03a00ae64c4a56a9cc0
Mufaqar Islam <mufaqar@gmail.com>
2023-02-15 20:04:47 +0500

    customer

--- temp-test.php
+++ temp-test.php
@@ -15,10 +15,12 @@ $customers = $stripe->customers->all([
   'email' => $email,
 ]);
 
-echo $customers['data'][0]['id'];
+$customer = $customers['data'][0]['id'];
+
+echo $customer;
 
 print "<pre>";
-print_r($customers);
+//print_r($customers);
 
 
 // $customers = \Stripe\Customer::all(['email' => $email, 'limit' => 1]);
@@ -31,48 +33,53 @@ print_r($customers);
 
 
 
+$intent = $stripe->charges->create([
+  'customer' => $customer, // replace with the actual customer ID
+  'amount' => 1500,
+  'currency' => 'nok',
+  'description' => 'Test Charge Custoer',
+]);
+
+//print_r($intent);
+
+$status = $intent->status;
+
+
+echo $status;
 
 
 
-  die("4515166");
 
 
 
 
-
-
-
-
-
-
-
-$method =  $stripe->paymentMethods->create([
-  'type' => 'card',
-  'card' => [
-    'number' => '4242424242424242',
-    'exp_month' => 11,
-    'exp_year' => 2023,
-    'cvc' => '314',
-  ],
-  ]);
-   $customer_added = $stripe->paymentIntents->create(
-  array(
-     'amount' => intval($grand_total),
-     'currency' => 'NOK',      
-    'payment_method_types' => array('card'),
-    'payment_method' => $method->id,
-    'customer' => 'cus_MlTVknOyPYZluK',
-    'description' => "Order Week : ".$inovice_week." Order Year :".$inovice_year
+// $method =  $stripe->paymentMethods->create([
+//   'type' => 'card',
+//   'card' => [
+//     'number' => '4242424242424242',
+//     'exp_month' => 11,
+//     'exp_year' => 2023,
+//     'cvc' => '314',
+//   ],
+//   ]);
+//    $customer_added = $stripe->paymentIntents->create(
+//   array(
+//      'amount' => intval($grand_total),
+//      'currency' => 'NOK',      
+//     'payment_method_types' => array('card'),
+//     'payment_method' => $method->id,
+//     'customer' => 'cus_MlTVknOyPYZluK',
+//     'description' => "Order Week : ".$inovice_week." Order Year :".$inovice_year
     
     
-    )
-  );
-  $confirm_payment = $stripe->paymentIntents->confirm(
-  $customer_added->id,
-  ['payment_method' => 'pm_card_visa']
-  );						  
+//     )
+//   );
+//   $confirm_payment = $stripe->paymentIntents->confirm(
+//   $customer_added->id,
+//   ['payment_method' => 'pm_card_visa']
+//   );						  
 
-    $status = $confirm_payment->status;
+             // $status = $confirm_payment->status;
               add_post_meta($invoice_id, 'invoice_status',$status, true);
               add_post_meta($invoice_id, 'payment_message', $confirm_payment, true);
 
